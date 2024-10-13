a4a758b515b1ec4e0b688f5008d067b277630432
Marius Naasen <mariusnaasen@gmail.com>
2024-10-13 11:16:37 +0200

    Added function prototypes and doc in matrix.h

--- src/shared/matrix.h
+++ src/shared/matrix.h
@@ -16,7 +16,33 @@ typedef struct {
 
 } Matrix;
 
-Matrix* matrix_create(size_t num_rows, size_t num_cols);
+/*
+ * @brief Create a Matrix from a static array given by the user.
+ *
+ * @param1 The number of rows in the Matrix.
+ * @param2 The number of columns in the Matrix.
+ * @param3 The array containg the values of the Matrix.
+ * @return A Matrix object.
+*/
+Matrix* matrix_create_from_array(
+    size_t num_rows,
+    size_t num_cols,
+    int arr[num_rows][num_cols]
+);
+
+/*
+ * @brief Create a Matrix from a pointer of pointers (int**).
+ *
+ * @param1 The number of rows in the Matrix.
+ * @param2 The number of columns in the Matrix.
+ * @param3 A pointer to the allocated array containing the values of the Matrix.
+ * @return A Matrix object.
+*/
+Matrix* matrix_create_from_pointers(
+    size_t num_rows,
+    size_t num_cols,
+    int** rows
+);
 
 /*
  * @brief Create a Matrix containing all zeros.
