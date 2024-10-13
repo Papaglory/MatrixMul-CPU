3666ea173b7f63f59e250bb9755b99fd23888ba0
Marius Naasen <mariusnaasen@gmail.com>
2024-10-13 10:48:24 +0200

    Implemented matrix_create_zero()

---
+++ src/shared/matrix.c
@@ -0,0 +1,68 @@
+#include <errno.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include "matrix.h"
+
+Matrix* matrix_create(size_t num_rows, size_t num_cols) {
+
+    // Both dimensions has to be at least greater than 0
+    if (num_rows == 0 || num_cols == 0) { errno = EINVAL; return NULL; }
+
+    return NULL;
+}
+
+Matrix* matrix_create_zero(size_t num_rows, size_t num_cols) {
+
+    // Both dimensions has to be at greater than 0
+    if (num_rows == 0 || num_cols == 0) { errno = EINVAL; return NULL; }
+
+    // Allocate memory for the row pointers
+    int** rows = (int**)malloc(num_rows * sizeof(int*));
+    if (!rows) { errno = ENOMEM; return NULL; }
+
+    // Allocate each row in rows
+    size_t i = 0;
+    for (i = 0; i < num_rows; i++) {
+        rows[i] = (int*)malloc(num_cols * sizeof(int));
+        if (!rows[i]) {
+            // Memory allocation failed
+            errno = ENOMEM;
+            break;
+        }
+
+        // Initialize row values to 0
+        for (size_t j = 0; j < num_cols; j++) {
+            rows[i][j] = 0;
+        }
+    }
+
+    // Check if the loop completed successfully
+    if (i != num_rows) {
+        // Free everything that was allocated
+        for (size_t j = 0; j < i; j++) {
+            free(rows[j]);
+        }
+        free(rows);
+        return NULL;
+    }
+
+    // Create the matrix
+    Matrix* m = (Matrix*)malloc(sizeof(Matrix));
+    if (!m) {
+        // free allocated rows before returning
+        for (size_t i = 0; i < num_rows; i++) {
+            free(rows[i]);
+        }
+        free(rows);
+
+        errno = ENOMEM;
+        return NULL;
+    }
+
+    // Set Matrix member variables
+    m->rows = rows;
+    m->num_rows = num_rows;
+    m->num_cols = num_cols;
+
+    return m;
+}
--- src/shared/matrix.h
+++ src/shared/matrix.h
@@ -4,7 +4,6 @@
 #include <stddef.h>
 
 typedef struct {
-
     /*
      * A pointer pointing to a pointer that points to an integer.
      * The idea of an array on the heap is handled through the
@@ -15,12 +14,21 @@ typedef struct {
     size_t num_rows;
     size_t num_cols;
 
-} matrix;
+} Matrix;
 
-matrix* matrix_create();
+Matrix* matrix_create(size_t num_rows, size_t num_cols);
 
-matrix* matrix_add(void* matrix_1, void* matrix_2);
+/*
+ * @brief Create a Matrix containing all zeros.
+ *
+ * @param1 The number of rows in the Matrix.
+ * @param2 The number of columns in the Matrix.
+ * @return A Matrix object.
+*/
+Matrix* matrix_create_zero(size_t num_rows, size_t num_cols);
 
-matrix* matrix_mult(void* matrix_1, void* matrix_2);
+Matrix* matrix_add(void* matrix_1, void* matrix_2);
+
+Matrix* matrix_mult(void* matrix_1, void* matrix_2);
 
 #endif // MATRIX_H
