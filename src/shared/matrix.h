9ee2a47e10b97d115f3ebd52ec03dc63724b16bf
Marius Naasen <mariusnaasen@gmail.com>
2024-10-13 19:59:07 +0200

    Added doc to matrix.h

--- src/shared/matrix.h
+++ src/shared/matrix.h
@@ -61,9 +61,33 @@ Matrix* matrix_create_from_pointers(
 */
 Matrix* matrix_create_zero(size_t num_rows, size_t num_cols);
 
-Matrix* matrix_add(void* matrix_1, void* matrix_2);
+/**
+ * @brief Add the two matrices by adding the elements together.
+ * Assumes the matrices have the same dimensions. The result
+ * is placed in a newly allocated Matrix.
+ * Vanilla means it is single threaded on the CPU.
+ *
+ * @param m1 The first Matrix.
+ * @param m2 The second Matrix.
+ * @return A pointer to a newly alloced Matrix which contains the result
+ * of the additon. Returns NULL if an error occured.
+*/
+Matrix* matrix_vanilla_add(void* m1, void* m2);
 
-Matrix* matrix_mult(void* matrix_1, void* matrix_2);
+/**
+ * @brief Matrix multiply the two matrices. Matrix 'm1' is the
+ * left-Matrix and Matrix 'm2' is the right-Matrix.
+ * Assumes the number of columns in the first Matrix is equal
+ * to the number of rows in the second Matrix.
+ * The result is placed in a newly allocated Matrix.
+ * Vanilla means it is single threaded on the CPU.
+ *
+ * @param m1 The first Matrix.
+ * @param m2 The second Matrix.
+ * @return A pointer to a newly alloced Matrix which contains the result
+ * of the multiplication. Returns NULL if an error occured.
+*/
+Matrix* matrix_vanilla_mult(void* m1, void* m2);
 
 /**
  * @brief Print the given Matrix for visualization or debugging.
