6d858e3ece15616ebf73afdddd14834e11b4040d
Marius Naasen <mariusnaasen@gmail.com>
2024-10-13 12:27:30 +0200

    Implemented matrix_create_from_array(). Refactored matrix_create_zero()

--- src/shared/matrix.c
+++ src/shared/matrix.c
@@ -3,16 +3,36 @@
 #include <stdbool.h>
 #include "matrix.h"
 
-Matrix* matrix_create_from_array(
-    size_t num_rows,
-    size_t num_cols,
-    int arr[num_rows][num_cols]
-);
+/**
+ * @brief Given a pointer that represents an array of arrays / 2D array
+ * of type (int**), free each row / array and finally free the 'rows' pointer.
+ *
+ * @param num_rows The number of rows in the 2D array.
+ * @param rows The pointer pointing to the array of arrays / 2D array.
+ * @return A zero to indicate success, and -1 if an error occured.
+*/
+int free_rows(size_t num_rows, int** rows) {
 
-Matrix* matrix_create_zero(size_t num_rows, size_t num_cols) {
+    // Check for valid arguments
+    if (num_rows == 0 || !rows) { errno = EINVAL; return -1; }
 
-    // Both dimensions has to be at greater than 0
-    if (num_rows == 0 || num_cols == 0) { errno = EINVAL; return NULL; }
+    for (size_t i = 0; i < num_rows; i++) {
+        free(rows[i]);
+    }
+    free(rows);
+    return 0;
+}
+
+/**
+ * @brief Given the input dimensions, allocate an array of arrays / 2D array
+ * on the heap.
+ *
+ * @param num_rows The number of rows in the 2D array.
+ * @param num_cols The number of cols in the 2D array.
+ * @return A pointer to the allocated 2D array on the heap, and NULL
+ * if an error occured.
+*/
+int** allocate_rows(size_t num_rows, size_t num_cols) {
 
     // Allocate memory for the row pointers
     int** rows = (int**)malloc(num_rows * sizeof(int*));
@@ -27,33 +47,72 @@ Matrix* matrix_create_zero(size_t num_rows, size_t num_cols) {
             errno = ENOMEM;
             break;
         }
+    }
 
-        // Initialize row values to 0
+    // Check if the loop completed successfully
+    if (i != num_rows) {
+        // Free the rows that was allocated
+        free_rows(i, rows);
+        return NULL;
+    }
+
+    return rows;
+}
+
+Matrix* matrix_create_from_array(size_t num_rows, size_t num_cols,
+                                int values[num_rows][num_cols]) {
+
+    // Both dimensions has to be at greater than 0
+    if (num_rows == 0 || num_cols == 0) { errno = EINVAL; return NULL; }
+
+    // Allocate a 2D array with size determined by 'num_rows', 'num_cols'
+    int** rows = allocate_rows(num_rows, num_cols);
+    if (!rows) { return NULL; }
+
+    // Initialize row values according to user-array
+    for (size_t i = 0; i < num_rows; i++) {
+        for (size_t j = 0; j < num_cols; j++) {
+            rows[i][j] = values[i][j];
+        }
+    }
+
+    // Create the Matrix
+    Matrix* m = (Matrix*)malloc(sizeof(Matrix));
+    if (!m) {
+        // free allocated rows before returning
+        free_rows(num_rows, rows);
+        return NULL;
+    }
+
+    // Set Matrix member variables
+    m->rows = rows;
+    m->num_rows = num_rows;
+    m->num_cols = num_cols;
+
+    return m;
+}
+
+Matrix* matrix_create_zero(size_t num_rows, size_t num_cols) {
+
+    // Both dimensions has to be at greater than 0
+    if (num_rows == 0 || num_cols == 0) { errno = EINVAL; return NULL; }
+
+    // Allocate a 2D array with size determined by 'num_rows', 'num_cols'
+    int** rows = allocate_rows(num_rows, num_cols);
+    if (!rows) { return NULL; }
+
+    // Initialize row values to zeros
+    for (size_t i = 0; i < num_rows; i++) {
         for (size_t j = 0; j < num_cols; j++) {
             rows[i][j] = 0;
         }
     }
 
-    // Check if the loop completed successfully
-    if (i != num_rows) {
-        // Free everything that was allocated
-        for (size_t j = 0; j < i; j++) {
-            free(rows[j]);
-        }
-        free(rows);
-        return NULL;
-    }
-
-    // Create the matrix
+    // Create the Matrix
     Matrix* m = (Matrix*)malloc(sizeof(Matrix));
     if (!m) {
         // free allocated rows before returning
-        for (size_t i = 0; i < num_rows; i++) {
-            free(rows[i]);
-        }
-        free(rows);
-
-        errno = ENOMEM;
+        free_rows(num_rows, rows);
         return NULL;
     }
 
