e9b5327e67945e943309851b12e9bd9dec4e073f
Marius Naasen <mariusnaasen@gmail.com>
2024-10-19 14:04:26 +0200

    Tested with simple tests in matrix_mult_single_test.c

--- src/cpu/matrix_singlethread.c
+++ src/cpu/matrix_singlethread.c
@@ -7,7 +7,7 @@
 /**
  * @brief Determines the smallest of the two input integer values.
  * @param a The first integer.
- * @param b The first integer.
+ * @param b The second integer.
  *
  * @return The integer that is the smallest.
 */
@@ -17,64 +17,75 @@ int min(int a, int b) {
 
 Matrix* mult(Matrix* A, Matrix* B, size_t block_size) {
 
-    // Extract Matrix dimensions
-    size_t n = A->num_rows;
-    size_t m = A->num_cols;
-    size_t p = B->num_cols;
-
     if (!A || !B) {
-        // Parameters missing
         errno = EINVAL;
         perror("Error: Missing either Matrix A or Matrix B");
         return NULL;
     }
 
+    // Extract Matrix dimensions
+    size_t n = A->num_rows;
+    size_t m = A->num_cols;
+    size_t p = B->num_cols;
+
     if (n == 0 || m == 0 || p == 0) {
-        // Invalid Matrix dimensions
         errno = EINVAL;
         perror("Error: At least one of the dimensions (n, m or p) are 0");
         return NULL;
     }
 
-    if (block_size == 0 || block_size > n || block_size > m || block_size > p) {
-        // Invalid block size
+    size_t temp = min(n, m);
+    if (block_size == 0 || block_size > min(temp, p)) {
         errno = EINVAL;
         perror("Error: An invalid block size has been chosen");
         return NULL;
     }
 
     Matrix* C = matrix_create_with(pattern_zero, NULL, n, p);
-    if (!C) { return NULL; }
+    if (!C) {
+      errno = EINVAL;
+      perror("Error: Unable to allocate memory for Matrix C");
+      return NULL;
+    }
 
+    // retrieve internal Matrix arrays
     int* A_arr = A->values;
     int* B_arr = B->values;
     int* C_arr = C->values;
 
     // Iterate over blocks of Matrix C
     for (size_t i = 0; i < n; i += block_size) {
-        // Precompute upper bound to make sure we are
-        // either inside the block if it is full or inside the matrix if block is partial
+        /* Precompute upper bound to make sure we are
+         * either inside the block if it is full or inside
+         * the matrix if block is partial.
+        */
         size_t i_max = min(i + block_size, n);
 
         for (size_t j = 0; j < p; j += block_size) {
             // Precompute upper bound
             size_t j_max = min(j + block_size, p);
 
-            // Iterate over blocks in the shared dimension (rows of A and columns of B)
+            /*
+            * Iterate over blocks in the shared dimension
+            * (rows of A and columns of B).
+            */
             for (size_t k = 0; k < m; k += block_size) {
                 // Precompute upper bound
                 size_t k_max = min(k + block_size, m);
 
                 // Iterate over elements within the current block of C
                 for (size_t ii = i; ii < i_max; ii++) {
-                    for (int jj = j; jj < j_max; jj++) {
+                    for (size_t jj = j; jj < j_max; jj++) {
 
-                        // Calculate the dot product for a single element in the C block
-                        // Precompute the index for the current element in matrix C to avoid redundant calculations in the innermost loop
+                        /*
+                        * Calculate the dot product for a single element
+                        * in the C block. Indices have been precomputed
+                        * to avoid redundant calculations in the inner loop.
+                        */
                         size_t kk = 0;
                         size_t c_index = ii * p + jj;
                         size_t a_row_offset = ii * m;
-                        for (kk = k; kk < k_max - 3; kk += 4) {
+                        for (kk = k; kk + 3 < k_max; kk += 4) {
                             C_arr[c_index] += A_arr[a_row_offset + kk] * B_arr[kk * p + jj];
                             C_arr[c_index] += A_arr[a_row_offset + (kk + 1)] * B_arr[(kk + 1) * p + jj];
                             C_arr[c_index] += A_arr[a_row_offset + (kk + 2)] * B_arr[(kk + 2) * p + jj];
@@ -89,6 +100,6 @@ Matrix* mult(Matrix* A, Matrix* B, size_t block_size) {
         }
     }
 
-  // Return the result
-  return C;
+    // Return the result
+    return C;
 }
--- tests/matrix_mult_single_test.c
+++ tests/matrix_mult_single_test.c
@@ -1,21 +1,34 @@
 #include "../src/shared/matrix.h"
 #include "../src/cpu/matrix_singlethread.h"
 #include <stdio.h>
-#include <stdlib.h>
 
 int main() {
 
     printf("%s\n", "--------STARTING matrix_mult_single_test.c--------");
 
     printf("%s\n", "Creating a Matrix from array on the stack");
-    int arr[2][2] = {
-        {1,2},
-        {12,4}
+    int arr1[3][3] = {
+        {1,3,2},
+        {5,3,2},
+        {1,0,1}
     };
-    Matrix* m2 = matrix_create_from_2D_array(2, 2, arr);
+    Matrix* m1 = matrix_create_from_2D_array(3, 3, arr1);
+    printf("%s\n", "Matrix1");
+    matrix_print(m1);
+
+    int arr2[3][2] = {
+        {1,0},
+        {0,1},
+        {0,0}
+    };
+    printf("%s\n", "Matrix2");
+    Matrix* m2 = matrix_create_from_2D_array(3, 2, arr2);
     matrix_print(m2);
 
-
+    size_t block_size = 1;
+    printf("%s\n", "Calling matrix mult");
+    Matrix* result = mult(m1, m2, block_size);
+    matrix_print(result);
 
     return 0;
 }
