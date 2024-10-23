107cbc5527354535b0db2e2177e32def3ac4e1ad
Marius Naasen <mariusnaasen@gmail.com>
2024-10-23 10:36:51 +0200

    Implemented task.h

--- src/shared/task.h
+++ src/shared/task.h
@@ -1,11 +1,23 @@
 #ifndef TASK_H
 #define TASK_H
 
+#include "matrix.h"
+
 typedef struct {
 
-    int foo;
+    // The matrices corresopnding to A x B = C.
+    Matrix* A;
+    Matrix* B;
+    Matrix* C;
 
+    // The block size to use (blocking method)
+    size_t block_size;
 
+    // Variables to describe the C block (start inclusive, end exclusive)
+    size_t C_row_start;
+    size_t C_col_start;
+    size_t C_row_end;
+    size_t C_col_end;
 
 } Task;
 
